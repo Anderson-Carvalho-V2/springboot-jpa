86e9b60b9d24d229a84f8180bf6a2a0d4ab7ab36
Anderson <andersson_73@live.com>
2023-06-15 15:25:38 -0300

    OrderStatus enum

--- src/main/java/com/capgemini/course/config/TestConfig.java
+++ src/main/java/com/capgemini/course/config/TestConfig.java
@@ -10,33 +10,34 @@ import org.springframework.context.annotation.Profile;
 
 import com.capgemini.course.entities.Order;
 import com.capgemini.course.entities.User;
+import com.capgemini.course.entities.enums.OrderStatus;
 import com.capgemini.course.repositories.OrderRepository;
 import com.capgemini.course.repositories.UserRepository;
 
 @Configuration
 @Profile(value = "test")
-public class TestConfig implements CommandLineRunner{
+public class TestConfig implements CommandLineRunner {
 
-	//database seeding
-	
+	// database seeding
+
 	@Autowired
 	private UserRepository userRepository;
 
 	@Autowired
 	private OrderRepository orderRepository;
-	
+
 	@Override
 	public void run(String... args) throws Exception {
-	
+
 		User u1 = new User(null, "Maria", "maria@email.com", "99999999", "12345");
 		User u2 = new User(null, "Joao", "joao@email.com", "99999999", "12345");
-		
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2019-06-21T03:42:10Z"), u2);
-		Order o3 = new Order(null, Instant.parse("2019-06-22T15:22:22Z"), u1);
-		
+
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2019-06-21T03:42:10Z"), OrderStatus.WAITING_PAIMENT, u2);
+		Order o3 = new Order(null, Instant.parse("2019-06-22T15:22:22Z"), OrderStatus.WAITING_PAIMENT, u1);
+
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
-		
-	}	
+
+	}
 }
